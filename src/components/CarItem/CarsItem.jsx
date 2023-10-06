85e138359385f6c1fde0dfd8464008d26588d3f5
PolinaTsugankova <pp6056512@gmail.com>
2023-10-07 00:20:48 +0300

    Adds images to CarList

--- src/components/CarItem/CarsItem.jsx
+++ src/components/CarItem/CarsItem.jsx
@@ -1,26 +1,42 @@
 import React from "react";
+import { Image, ImageWrapper } from "./CarsItem.styled";
 
 const CarsItem = ({ carInfo }) => {
-   //    const {
-   //       id,
-   //       year,
-   //       make,
-   //       model,
-   //       type,
-   //       img,
-   //       description,
-   //       fuelConsumption,
-   //       engineSize,
-   //       accessories,
-   //       functionalities,
-   //       rentalPrice,
-   //       rentalCompany,
-   //       address,
-   //       rentalConditions,
-   //       mileage,
-   //    } = carInfo;
+   const {
+      id,
+      year,
+      make,
+      model,
+      type,
+      img,
+      description,
+      fuelConsumption,
+      engineSize,
+      accessories,
+      functionalities,
+      rentalPrice,
+      rentalCompany,
+      address,
+      rentalConditions,
+      mileage,
+   } = carInfo;
 
-   return <div>CarsItem</div>;
+   console.log(img);
+
+   return (
+      <div>
+         <Image
+            src={img}
+            alt={model}
+            style={{ backgroundImage: `url(${img})` }}
+         />
+
+         <p>.</p>
+         <p></p>
+         <p>qweqweqwe | qweqwe</p>
+         <button>Learn more</button>
+      </div>
+   );
 };
 
 export default CarsItem;
---
+++ src/components/CarItem/CarsItem.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+   width: 274px;
+   height: 274px;
+   object-fit: cover;
+   border-radius: 10px;
+   background: linear-gradient(
+         180deg,
+         rgba(18, 20, 23, 0.5) 2.5%,
+         rgba(18, 20, 23, 0) 41.07%
+      ),
+      transparent;
+`;

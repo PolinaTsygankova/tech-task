e9e554728049176d80f93e3cf7f0e36ff3551ae2
PolinaTsugankova <pp6056512@gmail.com>
2023-10-11 23:30:49 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,8 +2,6 @@ export const allCars = (state) => state.cars.cars;
 
 export const favouriteCars = (state) => {
    const cars = allCars(state);
-
    const list = cars.filter((car) => car.favourite === true);
-
    return list;
 };
